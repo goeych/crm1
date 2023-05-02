25fafb2b4f643250ba4e2d5efd2c00967cecf291
goeych <110361567+goeych@users.noreply.github.com>
2023-05-02 21:29:45 +0800

    Add files via upload

--- crm1/settings.py
+++ crm1/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*(^6^n(=pl7i*!4man1hoho7yf6@5=++1a#7$zd%rayja#d8+n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
