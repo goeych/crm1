8e828a3e7042726a58b3487f9b6330c35845a79f
goeych <110361567+goeych@users.noreply.github.com>
2023-05-04 13:13:42 +0800

    Add files via upload

--- crm1/settings.py
+++ crm1/settings.py
@@ -122,7 +122,7 @@ MEDIA_URL = 'images/'
 STATICFILES_DIRS = 'whitenoise.storage.CompressedManifestStaticFilesStorage',
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
